dfc2b52a1ba4acf9c1a3f3648b6f0f102d8278bc
karenlha <karenlha98@gmail.com>
2016-12-14 23:05:13 -0600

    Add files via upload

---
+++ examen.py
@@ -0,0 +1,11 @@
+#Karen Lizbeth Herrera Avila
+def impar(n):
+lista=int(input(lista de enteros))
+enteros=[]
+impares=[]
+if n % 2 == 1:
+    for n in lista: 
+    enteros.append(n)
+    sorted(enteros)
+    impares=(sorted(enteros,reverse=True))
+    print impares((sorted(enteros,reverse=True)[0]))
